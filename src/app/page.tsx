9c7f1f0a9ce3bf166179f79bf6141eae4780e830
Harkirat Saluja <saluja.harkirat@gmail.com>
2025-04-29 11:36:08 +0530

    feat: auth ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { Button } from "@/components/ui/button";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-    <div className="text-rose-500 font-bold text-2xl">
-      Hello world
-      <Button
-      >
-        Click me
-      </Button>
-    </div>
+    <AuthScreen />
   )
 }
