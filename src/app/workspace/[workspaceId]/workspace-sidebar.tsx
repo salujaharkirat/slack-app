a8cb1af8afd8b8b0ec676a25f088455e628ed458
Harkirat Saluja <saluja.harkirat@gmail.com>
2025-05-09 11:39:26 +0530

    feat: create channel api and ui to display

--- src/app/workspace/[workspaceId]/workspace-sidebar.tsx
+++ src/app/workspace/[workspaceId]/workspace-sidebar.tsx
@@ -8,9 +8,13 @@ import { useGetChannels } from "@/features/channels/api/use-get-channels";
 import { WorkspaceSection } from "./workspace-section";
 import { useGetMembers } from "@/features/members/api/use-get-members";
 import { UserItem } from "./user-item";
+import { useCreateChannelModal } from "@/features/channels/store/use-create-channel-modal";
 
 export const WorkspaceSidebar = () => {
   const workspaceId = useWorkspaceId();
+
+  const [, setOpen] = useCreateChannelModal();
+
   const { data: member, isLoading: memberLoading } = useCurrentMember({ workspaceId });
   const { data: workspace, isLoading: workspaceLoading } = useGetWorkspace({id: workspaceId});
   const { data: channels, isLoading: channelLoading } = useGetChannels({ workspaceId });
@@ -58,7 +62,7 @@ export const WorkspaceSidebar = () => {
       <WorkspaceSection
           label="Channels"
           hint="New channel"
-          onNew={() => {}}
+          onNew={member.role === "admin" ? () => setOpen(true) : () => null}
         >
           {
             channels?.map((item) => (
--- src/components/modals.tsx
+++ src/components/modals.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { CreateChannelModal } from "@/features/channels/components/create-channel-modal";
 import { CreateWorkspaceModal } from "@/features/workspaces/components/create-workspace-modal";
 import { useEffect, useState } from "react";
 
@@ -18,6 +19,7 @@ export const Modals = () => {
   return (
     <>
       <CreateWorkspaceModal />
+      <CreateChannelModal />
     </>
   )
 }
