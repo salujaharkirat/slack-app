12011e73f04a9f2765b296e4f178fc8016824400
Harkirat Saluja <saluja.harkirat@gmail.com>
2025-05-09 12:11:01 +0530

    feat: invite modal plus join code refresh

--- convex/workspaces.ts
+++ convex/workspaces.ts
@@ -2,6 +2,39 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 
+export const newJoinCode = mutation({
+  args: {
+    workspaceId: v.id("workspaces")
+  },
+  handler: async(ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    
+    if (!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) => 
+        q
+        .eq("workspaceId", args.workspaceId)
+        .eq("userId", userId)
+      )
+      .unique();
+    
+    if (!member || member.role !== "admin") {
+      throw new Error("unauthorized");
+    }
+
+    const newCode = generateCode();
+    await ctx.db.patch(args.workspaceId, {
+      joinCode: newCode,
+    });
+
+    return args.workspaceId;
+  }
+})
+
 const generateCode = () => {
   const code = Array.from(
     {length: 6},
