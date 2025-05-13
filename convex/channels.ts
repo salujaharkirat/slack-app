f0e5a7a9be21530d3a646a5b1d8e03fffad5f167
Harkirat Saluja <saluja.harkirat@gmail.com>
2025-05-13 12:48:04 +0530

    feat: channel header

--- convex/channels.ts
+++ convex/channels.ts
@@ -2,6 +2,86 @@ import { getAuthUserId } from "@convex-dev/auth/server";
 import { mutation, query } from "./_generated/server";
 import { v } from "convex/values";
 
+
+export const remove = mutation({
+  args: {
+    id: v.id("channels")
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    
+    if(!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const channel = await ctx.db.get(args.id);
+
+    if (!channel) {
+      throw new Error("Channel not found");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q
+          .eq("workspaceId", channel.workspaceId)
+          .eq("userId", userId)
+      )
+      .unique();
+  
+    if (!member || member.role !== "admin") {
+      throw new Error("unauthorized");
+    }
+
+    await ctx.db.delete(args.id);
+
+    // TODO: Remove associated messages
+
+    return args.id;
+  }
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("channels"),
+    name: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+    
+    if(!userId) {
+      throw new Error("Unauthorized");
+    }
+
+    const channel = await ctx.db.get(args.id);
+
+    if (!channel) {
+      throw new Error("Channel not found");
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) =>
+        q
+          .eq("workspaceId", channel.workspaceId)
+          .eq("userId", userId)
+      )
+      .unique();
+  
+    if (!member || member.role !== "admin") {
+      throw new Error("unauthorized");
+    }
+
+    const parsedName = args.name.replace(/\s+g/, '-').toLowerCase();
+
+    await ctx.db.patch(args.id, {
+      name: parsedName,
+    })
+
+    return args.id;
+  }
+});
+
 export const create = mutation({
   args: {
     name: v.string(),
@@ -33,6 +113,38 @@ export const create = mutation({
     });
 
     return channelId;
+  }
+});
+
+export const getById = query({
+  args: {
+    id: v.id("channels")
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) {
+      return null;
+    }
+
+    const channel = await ctx.db.get(args.id);
+
+    if (!channel) {
+      return null;
+    }
+
+    const member = await ctx.db
+      .query("members")
+      .withIndex("by_workspace_id_user_id", (q) => 
+        q.eq("workspaceId", channel.workspaceId).eq("userId", userId),
+      )
+      .unique();
+
+    if (!member) {
+      return null;
+    }
+
+    return channel;
   }
 })
 
